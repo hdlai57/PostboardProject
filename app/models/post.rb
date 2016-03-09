c1e2def73601705d03fea1a1b1d84027fbc1acf3
hdlai57 <hdlai57@gmail.com>
2016-03-10 02:26:42 +0800

    add database relationships

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+
+  has_many :comments
+  has_many :categories_posts
+  has_many :categories, through: :categories_posts
+
+  validates :content, presence: true, length: {minimum: 10}
 end
