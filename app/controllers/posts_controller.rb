acacdf1e7f4ad6da5a11f6a163f6f9df13d0bc41
hdlai57 <hdlai57@gmail.com>
2016-03-11 18:29:06 +0800

    added show in posts controller" "

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,36 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comments = @post.comments
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    # add code to identify admin-user
+    post.user = User.first
+    binding.pry
+    
+    if post.save!
+      redirect_to posts_path
+    else 
+      render :new
+    end
+
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  get 'posts/new'
 
   get 'posts/show'
 
